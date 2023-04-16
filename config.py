a962e57e5bcf772f45b5c63d3d92320544365edc
Vinki <90899750+v1nki@users.noreply.github.com>
2023-04-16 14:41:43 +0500

    Update config.py

--- config.py
+++ config.py
@@ -11,10 +11,10 @@ class Color:
 
 class Auth:
     discord_auth = {
-        "debug": "MTA3MTAwMDczNTA3Mzk4MDQ0Ng.G1zDn7.7FYxOEf0Ck74zWX8wqjYu7Lj8dY8SgOwqtMzWs",
-        "release": "MTA2OTM3NzQ2MDM1MDc1ODk1Mg.G7V3pq.etPo5aEamYFuArb4TuYcC2Lf5fgpFF1H7dhvmU"
+        "debug": "",
+        "release": ""
     }
-    qiwi_auth = "eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6Inp0ajl5eS0wMCIsInVzZXJfaWQiOiI3OTgyMjc2MjE5OCIsInNlY3JldCI6IjcyMzcwNGU4OTk3Y2Y1NzdiMjczMjU2N2FiMjIwZmQ3ODJkYzZkZjUwNzU3MTJjMjY5YTE5OWU2MDk5YjhjM2EifX0="
+    qiwi_auth = ""
 
 class Other:
     shard_count = 1
